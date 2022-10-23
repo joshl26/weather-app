367f96c2d99bcf864c601e707e0e69316da65c14
joshl26 <joshl26@hotmail.com>
2022-10-23 00:21:42 -0400

    commit

--- client/src/Components/WeatherInfoPanel.jsx
+++ client/src/Components/WeatherInfoPanel.jsx
@@ -1,33 +1,71 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class WeatherInfoPanel extends Component {
+  // Differentiates whether user chose to use Celsius or Fahrenheit
+  getMetric = () => {
+    let metric = localStorage.getItem("tempMetric");
+    metric = !!metric ? metric : "";
+    return metric.includes("metric") ? "°C" : "°F";
+  };
+
   render() {
-    return (
-      <section className="weather-info">
-        <h3 className="city-name">City Name</h3>
+    const { weatherData } = this.props;
+    let metricSymbol = this.getMetric();
 
-        <section className="overcast">
-          <img src="" className="overcast-img" alt="" />
-          <span className="overcast-description">Weather Description</span>
+    if (Object.keys(weatherData).length > 0) {
+      const iconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+
+      return (
+        <section className="weather-info">
+          <h3 className="city-name">{weatherData.name}</h3>
+
+          <section className="overcast">
+            <img src="" className="overcast-img" alt="" />
+            <span className="overcast-description">
+              weatherData.weather[0].description
+            </span>
+          </section>
+
+          <hr />
+
+          <section className="current-weather">
+            <span className="humidity">
+              Humidity: {weatherData.main.humidity}%
+            </span>
+            <span className="curr-temp">
+              Temp: {weatherData.main.temp}
+              {metricSymbol}
+            </span>
+            <span className="feels-like">
+              Feels like: {weatherData.main.feels_like}
+              {metricSymbol}
+            </span>
+          </section>
+
+          <hr />
+
+          <section className="temps">
+            <span className="min-temp">
+              Low: {weatherData.main.temp_min}
+              {metricSymbol}
+            </span>
+            <span className="max-temp">
+              High: {weatherData.main.temp_max}
+              {metricSymbol}
+            </span>
+          </section>
         </section>
-
-        <hr />
-
-        <section className="current-weather">
-          <span className="humidity">Humidity: X%</span>
-          <span className="curr-temp">Temp: X°F</span>
-          <span className="feels-like">Feels like: X°F</span>
-        </section>
-
-        <hr />
-
-        <section className="temps">
-          <span className="min-temp">Low: X°F</span>
-          <span className="max-temp">High: X°F</span>
-        </section>
-      </section>
-    );
+      );
+    }
+    return <section>WeatherInfo!</section>;
   }
 }
 
-export default WeatherInfoPanel;
+const mapStateToProps = (state) => {
+  return {
+    weatherData: state.weather,
+  };
+};
+
+export default connect(mapStateToProps)(WeatherInfoPanel);
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const path = require("path");
+
 // use body parser to get data from POST requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +15,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use API routes from the api folder
 const apis = require("./api");
 app.use("/api", apis);
+
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
 // Connect to Mongo
 mongoose
